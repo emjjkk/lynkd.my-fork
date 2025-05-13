9bbed3c0c12e7ae50639f9f23b1e5495034c293a
Emmanuel A. <alabiemmanuel2937@gmail.com>
2025-05-13 11:33:17 +0000

    remove auth-error page props

--- app/auth/auth-error/page.tsx
+++ app/auth/auth-error/page.tsx
@@ -1,16 +1,8 @@
-export default function AuthErrorPage({
-  searchParams,
-}: {
-  searchParams?: {
-    message?: string;
-  };
-}) {
-  const errorMessage = searchParams?.message || 'Unknown error occurred';
-
+export default function AuthErrorPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Dang, something went wrong</h1>
-      <p className="text-red-500">{errorMessage}. Try again maybe?</p>
+      <p className="text-red-500">Try again maybe?</p>
     </div>
   );
 }
