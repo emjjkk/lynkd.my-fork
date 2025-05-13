3d07dbb2e353fe84eaddfda6485da7c7a902854f
Emmanuel A. <alabiemmanuel2937@gmail.com>
2025-05-13 11:28:27 +0000

    fix auth-error page

--- app/auth/auth-error/page.tsx
+++ app/auth/auth-error/page.tsx
@@ -1,15 +1,15 @@
-// app/auth/auth-error/page.tsx
+type Props = {
+  searchParams?: {
+    message?: string;
+  };
+};
 
-export default function AuthErrorPage({
-  searchParams,
-}: {
-  searchParams: { message?: string }
-}) {
+export default function AuthErrorPage({ searchParams }: Props) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Dang, something went wrong</h1>
       <p className="text-red-500">
-        {searchParams.message || 'Unknown error occurred'}. Try again maybe?
+        {searchParams?.message || 'Unknown error occurred'}. Try again maybe?
       </p>
     </div>
   );
